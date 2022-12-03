af908dc65a38733ce8e1a4ac870fdaae6546138a
Alex MacCaw <alex@alexmaccaw.com>
2022-12-03 19:37:16 +1300

    Support block ref ids (#6)
    * Support block ref ids
    
    * refactor
    
    * add comments
    
    * add more tests

--- src/convertors/roam/roam-helpers.ts
+++ src/convertors/roam/roam-helpers.ts
@@ -29,7 +29,63 @@ export const toRoamId = (uid: string) => {
 
 // Takes a string like '[[Example]]' and returns ['Example']
 export const extractBacklinks = (str: string): string[] => {
-  const regex = /(\[\[[^\]]*\]\]|\(\([^)]*\)\))/g
+  const regex = /\[\[([^\]]+)\]\]/g
   const matches = str.match(regex) ?? []
   return matches.map((match) => match.slice(2, -2))
+}
+
+// Converts tags in the form of #[[tag]] or #tag to Wiki-style backlinks in the form of [[tag]].
+export const convertTagsToBacklinks = (str: string): string => {
+  return str.replace(/(^|\s)#\[\[/g, '$1[[').replace(/(^|\s)#([\w-]+)/g, '$1[[$2]]')
+}
+
+// Convert ((backlinks)) to [[backlinks]].
+export const convertBlockrefsToBacklinks = (str: string): string => {
+  return str.replace(/\(\(([^)]+)\)\)/g, '[[\\1]]')
+}
+
+export const extractTodos = (
+  str: string,
+): {checked: boolean | undefined; parsed: string} => {
+  const TODO_CHECKED = '{{DONE}}'
+  const TODO_CHECKED_ALT = '{{[[DONE]]}}'
+
+  const TODO_UNCHECKED = '{{TODO}}'
+  const TODO_UNCHECKED_ALT = '{{[[TODO]]}}'
+
+  if (str.startsWith(TODO_CHECKED_ALT)) {
+    return {checked: true, parsed: str.slice(TODO_CHECKED_ALT.length).trim()}
+  }
+
+  if (str.startsWith(TODO_CHECKED)) {
+    return {checked: true, parsed: str.slice(TODO_CHECKED.length).trim()}
+  }
+
+  if (str.startsWith(TODO_UNCHECKED_ALT)) {
+    return {checked: false, parsed: str.slice(TODO_UNCHECKED_ALT.length).trim()}
+  }
+
+  if (str.startsWith(TODO_UNCHECKED)) {
+    return {checked: false, parsed: str.slice(TODO_UNCHECKED.length).trim()}
+  }
+
+  return {
+    checked: undefined,
+    parsed: str,
+  }
+}
+
+export const normalizeNoteString = (noteString: string) => {
+  let string = noteString
+
+  // Convert roam tags (e.g. #tag) to backlinks (e.g. [[tag]])
+  string = convertTagsToBacklinks(string)
+
+  // Normalize the noteString by parsing out todos
+  const {checked, parsed: parsedTasksString} = extractTodos(string)
+
+  return {
+    checked,
+    markdown: parsedTasksString,
+  }
 }
