9e2405febcde93515234f3120b5bc1a2a498d76d
Dustin Mihalik <153942+mihalik@users.noreply.github.com>
2023-05-06 14:15:08 -0500

    Adding TODO example

--- src/convertors/logseq/logseq-convertor.test.ts
+++ src/convertors/logseq/logseq-convertor.test.ts
@@ -2,6 +2,7 @@ import {describe, it, expect} from 'vitest'
 
 import exampleExport from './fixtures/logseq-example.json'
 import examplePropertiesExport from './fixtures/logseq-property-example.json'
+import exampleTodoExport from './fixtures/logseq-todo-example.json'
 import {LogseqConvertor} from './logseq-convertor'
 
 describe('LogseqConvertor', () => {
@@ -14,6 +15,12 @@ describe('LogseqConvertor', () => {
   it('parses properties example', () => {
     const convertor = new LogseqConvertor({graphId: '123'})
     const {notes} = convertor.convert({data: JSON.stringify(examplePropertiesExport)})
+
+    expect(notes).toMatchSnapshot()
+  })
+  it('parses TODO example', () => {
+    const convertor = new LogseqConvertor({graphId: '123'})
+    const {notes} = convertor.convert({data: JSON.stringify(exampleTodoExport)})
 
     expect(notes).toMatchSnapshot()
   })
