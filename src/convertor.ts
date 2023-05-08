afd6ab0fcdee20e428b18296420ca3f5db14d37e
Alex MacCaw <alex@alexmaccaw.com>
2023-05-08 13:52:08 -0400

    Add logseq and mem

--- src/convertor.ts
+++ src/convertor.ts
@@ -1,3 +1,6 @@
+import {LogseqConvertor} from 'convertors/logseq'
+import {MemConvertor} from 'convertors/mem/mem-convertor'
+
 import {EvernoteConvertor} from './convertors/evernote'
 import {HtmlConvertor} from './convertors/html'
 import {MarkdownConvertor} from './convertors/markdown'
@@ -5,7 +8,15 @@ import {OpmlConvertor} from './convertors/opml'
 import {RoamConvertor} from './convertors/roam'
 import {Convertor} from './types'
 
-export type Format = 'evernote' | 'html' | 'markdown' | 'roam' | 'opml' | 'html'
+export type Format =
+  | 'evernote'
+  | 'html'
+  | 'html'
+  | 'logseq'
+  | 'markdown'
+  | 'mem'
+  | 'opml'
+  | 'roam'
 
 export const getConvertorForFormat = ({
   graphId,
@@ -15,16 +26,20 @@ export const getConvertorForFormat = ({
   format: Format
 }): Convertor => {
   switch (format) {
-    case 'roam':
-      return new RoamConvertor({graphId})
     case 'evernote':
       return new EvernoteConvertor()
-    case 'markdown':
-      return new MarkdownConvertor({graphId})
     case 'html':
       return new HtmlConvertor()
+    case 'logseq':
+      return new LogseqConvertor({graphId})
+    case 'markdown':
+      return new MarkdownConvertor({graphId})
+    case 'mem':
+      return new MemConvertor({graphId})
     case 'opml':
       return new OpmlConvertor()
+    case 'roam':
+      return new RoamConvertor({graphId})
     default:
       throw new Error('Unknown import format')
   }
